5b78758c7afcfa286048336e79f5ba1805304225
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-19 19:36:45 +0700

    add frontend

---
+++ docs/_next/static/chunks/bcd3eb96a1bb2921.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/ebe65ec0409fcbdb.js",
+  "static/chunks/3719191054898a8b.js",
+  "static/chunks/247d08b7389eb626.js",
+  "static/chunks/turbopack-333438a915f33c21.js"
+])
---
+++ docs/_next/static/yc_4EcS3VUHHDlWWYGJlu/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/bcd3eb96a1bb2921.js"
+  ],
+  "/_error": [
+    "static/chunks/c9f4ce248e090e1d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
