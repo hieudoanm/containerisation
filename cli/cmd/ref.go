36a9501a5823e663023c38cfa4e0491cc289f0a6
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-19 19:14:35 +0700

    initial

---
+++ cli/cmd/ref.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"doi-cli/utils/apa"
+	"doi-cli/utils/requests"
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// refCmd represents the ref command
+var refCmd = &cobra.Command{
+	Use:   "ref",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Get Id
+		fmt.Print("ID: ")
+		var id string
+		fmt.Scanln(&id)
+		var url string = fmt.Sprintf("https://api.crossref.org/works/%s", id)
+		response, err := requests.Get(url, requests.Options{})
+		if err != nil {
+			fmt.Println("Error Response:", err)
+			return
+		}
+		data := apa.CrossRefData{}
+		err = json.Unmarshal(response, &data)
+		if err != nil {
+			fmt.Println("Error Unmarshal:", err)
+			return
+		}
+		apa.PrintReference(data)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(refCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// refCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// refCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
