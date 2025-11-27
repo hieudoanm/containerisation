9208e3e04acb373af549efcacb209026fc3753e6
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-27 19:30:56 +0700

    add mobile

--- docs/_next/static/chunks/1f8d440c817d1f5c.js
+++ docs/_next/static/chunks/1f8d440c817d1f5c.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/96cdf3c1b6cece20.js",
   "static/chunks/64ca4d6f95bfed47.js",
   "static/chunks/3d98c1050ca4300c.js",
-  "static/chunks/f488c455b517e5af.css",
-  "static/chunks/turbopack-95db80727e99ec8b.js"
+  "static/chunks/40057ac6533e3374.css",
+  "static/chunks/turbopack-d1a66d1ed60f78a2.js"
 ])
---
+++ packages/frontend/mobile/android/app/src/main/java/io/github/hieudoanm/doi/MainActivity.java
@@ -0,0 +1,5 @@
+package io.github.hieudoanm.doi;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
